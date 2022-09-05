a70040a9a861d725a8849a1a1341f9d5f5d704a8
loven2005 <iamlovensaini2005@gmail.com>
2022-09-05 21:36:03 +0530

    updated login.py

--- login.py
+++ login.py
@@ -1,7 +1,12 @@
 import os
+def main():
+    f=open("data.txt")
+    for x in f:
+        print(x)
 print("HELLO USER!")
 print("Press 1 for Sign In OR press 2 for SIGN UP(New User)")
 ch=int(input())
+call=False
 if ch==1:#EXISTING USER SIGN IN
     print("Enter Username:",end=" ")
     user=input()
@@ -12,7 +17,11 @@ if ch==1:#EXISTING USER SIGN IN
     for x in f:
         if x==cu:
             print("ACCESS GRANTED")
+            main()
+            call=True
         else:
+            print(x)
+        if call==False:
             print("WRONG CREDENTIALS")
     f.close()
 elif ch==2: #NEW USER SIGN UP
@@ -22,12 +31,12 @@ elif ch==2: #NEW USER SIGN UP
     print("Enter Password:",end=" ")
     pw=input()
     cu=user+" "+pw
-    for x in f:
+    for x in f:#in caSE user account exisTs
         if x==cu:
             print("YOUR ACCOUNT ALREADY EXISTS!")
     f=open("data.txt","at")
+    f.write('\n')
     f.write(cu)
     f.close()
 else:
     print("Wrong Choice!")
-
