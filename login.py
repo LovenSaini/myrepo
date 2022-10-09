c5a753e11fb90a49c589c9a48cf20a6c553d8773
loven2005 <iamlovensaini2005@gmail.com>
2022-10-09 11:11:12 +0530

    fixed bug where logins were not accepted

--- login.py
+++ login.py
@@ -3,8 +3,9 @@ def main():
     f=open("data.txt")
     for x in f:
         print(x)
+    f.close()
 print("HELLO USER!")
-print("Press 1 for Sign In OR press 2 for SIGN UP(New User)")
+print("Press 1 for Sign In OR press 2 for SIGN UP(New User):",end=" ")
 ch=int(input())
 call=False
 if ch==1:#EXISTING USER SIGN IN
@@ -13,30 +14,35 @@ if ch==1:#EXISTING USER SIGN IN
     print("Enter Password:",end=" ")
     pw=input()
     f=open("data.txt","rt")
-    cu=user+" "+pw              
-    for x in f:
-        if x==cu:
+    fl=f.readlines()
+    cu="id:"+user+" "+"pw:"+pw+"\n"              
+    for x in fl:
+        if x==cu:#check id and pw
             print("ACCESS GRANTED")
             main()
             call=True
-        else:
-            print(x)
-        if call==False:
-            print("WRONG CREDENTIALS")
+            break
+        
+    if call==False:
+         print("WRONG CREDENTIALS")
     f.close()
 elif ch==2: #NEW USER SIGN UP
+    exisTs=False
     f=open("data.txt","rt")
     print("Enter Username to sign up:",end=" ")
     user=input()
     print("Enter Password:",end=" ")
     pw=input()
-    cu=user+" "+pw
+    cu="id:"+user+" "+"pw:"+pw+"\n"
     for x in f:#in caSE user account exisTs
         if x==cu:
             print("YOUR ACCOUNT ALREADY EXISTS!")
-    f=open("data.txt","at")
-    f.write('\n')
-    f.write(cu)
-    f.close()
+            exisTs=True
+            break
+    if exisTs==False:
+        f=open("data.txt","at")
+        f.write(cu)
+        f.close()
+        print("Account successfuly created.")
 else:
     print("Wrong Choice!")
